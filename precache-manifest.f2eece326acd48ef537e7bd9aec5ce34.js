beb197d4f1702353692fe5775de8c4383749f63b
Francisco Goya <degoyafran@gmail.com>
2020-04-07 12:51:37 -0500

    Updates

---
+++ precache-manifest.f2eece326acd48ef537e7bd9aec5ce34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9c25af170c67d80e736742164dd54c",
+    "url": "/19-user-directory/index.html"
+  },
+  {
+    "revision": "caeb56d7f12522c9a905",
+    "url": "/19-user-directory/static/css/main.28ccac9c.chunk.css"
+  },
+  {
+    "revision": "71d1ce77a5f2c3c495ba",
+    "url": "/19-user-directory/static/js/2.3d8140d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/19-user-directory/static/js/2.3d8140d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caeb56d7f12522c9a905",
+    "url": "/19-user-directory/static/js/main.d252993a.chunk.js"
+  },
+  {
+    "revision": "8040a2cf87019c416259",
+    "url": "/19-user-directory/static/js/runtime-main.34f38da3.js"
+  }
+]);
